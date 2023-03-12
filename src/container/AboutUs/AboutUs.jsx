5e56b99e073ff73e09620ee5bc74c3a59dfe0467
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-03-12 22:54:53 +0530

    aboutus & footer container component

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+// styles - aboutUs
+import "./AboutUs.css";
+import { images } from "../../constants";
+
+/*********************************
+ *
+ * AboutUs component
+ *
+ ********************************/
+const AboutUs = () => (
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          At our restaurant, we're passionate about sharing the diverse flavors
+          and cultural heritage of Australia through our carefully crafted
+          dishes. Drawing on the country's rich history and culinary traditions.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Our restaurant draws inspiration from Australia's rich history,
+          blending Indigenous and European influences to create a unique dining
+          experience that honors the country's diverse cultural heritage.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
