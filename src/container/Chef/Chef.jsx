4e65e8b1ebd14a92afd27a3c03a7e2fc4db4c79b
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-03-13 08:56:19 +0530

    chef container component added

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+// styles - chef
+import "./Chef.css";
+
+// components & constants
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+
+/**************************************
+ *
+ * Chef Component
+ *
+ *************************************/
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">
+            Taste exquisite cuisine from our passionate chef!.
+          </p>
+        </div>
+        <p className="p__opensans">
+          {" "}
+          Our talented chef brings a wealth from farm to table, our dedicated
+          chef masterfully crafts each dish with fresh, seasonal ingredients,
+          delivering an unforgettable dining experience for our guests.{" "}
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
